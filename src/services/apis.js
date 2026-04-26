3b8502614cfa9a5f1033b28123a4fb26bf128ca6
Hemraj Saini <hemrajsaini4772@gmail.com>
2026-04-26 15:00:26 +0530

    Update BASE_URL to include API version

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://studynotion-project-x0m0.onrender.com"
+const BASE_URL = "https://studynotion-project-x0m0.onrender.com/api/v1"
 
 // AUTH ENDPOINTS
 export const endpoints = {
