129756d3e3987f0898cc711d6cf02b16474c8cff
Hemraj Saini <hemrajsaini4772@gmail.com>
2026-04-26 15:46:41 +0530

    Refactor CORS origin configuration to array format

--- server/index.js
+++ server/index.js
@@ -23,8 +23,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
-		"https://studynotion-project-1-r57e.onrender.com",
+		 origin: [
+      "http://localhost:3000",
+      "https://studynotion-project-1-r57e.onrender.com",
+    ],
 		credentials:true,
 	})
 )
